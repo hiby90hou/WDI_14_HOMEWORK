43abbc084431eb6656bdffd89cca3341f0c7eb50
darkend <hiby.90hou@qq.com>
2018-01-31 23:30:40 +1100

    add extra homework

---
+++ Chang/wk08/3-wed/picshop/app.js
@@ -0,0 +1,34 @@
+console.log("work")
+
+var container = document.querySelector(".container");
+var inputBox = document.querySelector(".inputBox");
+var clickCounter = 0;
+
+	function fetchData(){
+		console.log('click')
+		console.log('http://api.giphy.com/v1/gifs/search?q='+inputBox.value+'&api_key=' + config.GIPHY_API + '&limit=10&offset='+clickCounter*10)
+		fetch('http://api.giphy.com/v1/gifs/search?q='+inputBox.value+'&api_key=' + config.GIPHY_API + '&limit=10&offset='+clickCounter*10)
+	  .then(function(response) {
+	    return response.text()
+	  })
+	  .then(function(data) {
+	  	var picList = JSON.parse(data).data;
+	  	picList.forEach(function(pic){
+		  	var newPic = document.createElement('img');
+		    newPic.src = pic.images.original.url;
+		    console.log(pic.images.original.url);
+		    container.appendChild(newPic);  			
+			})
+			clickCounter +=1;
+		})
+	}
+
+document.querySelector("#searchButton").addEventListener("click",fetchData);
+
+window.onscroll = function(){
+	console.log("scrollsize="+(window.pageYOffset + window.innerHeight))
+	console.log("pagesize="+(document.documentElement.scrollHeight))
+	if(Math.ceil(window.pageYOffset + window.innerHeight) >= document.documentElement.scrollHeight){
+		fetchData();
+	}
+}
