1d38dc5ca5cc3196caca1ca8b2bd6ce521c07559
darkend <hiby.90hou@qq.com>
2018-02-01 21:57:34 +1100

    add basic version

---
+++ Chang/wk08/4-thu/pokedex_app/app/controllers/pokemons_controller.rb
@@ -0,0 +1,18 @@
+class PokemonsController < ApplicationController
+	def index
+		@pokemon_list = Pokemon.all
+	end
+
+	def new
+	end
+
+	def create
+		new_pokemon = Pokemon.new
+		new_pokemon.species = params[:species]
+		new_pokemon.nickname = params[:nickname]
+		new_pokemon.level = params[:level]
+		new_pokemon.save
+
+		redirect_to '/'
+	end
+end
