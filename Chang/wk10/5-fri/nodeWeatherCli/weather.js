f5eaaecefea17163ceb2e5e43c3d0fcb839d6db8
darkend <hiby.90hou@qq.com>
2018-02-16 15:23:04 +1100

    hide api key to env value

--- Chang/wk10/5-fri/nodeWeatherCli/weather.js
+++ Chang/wk10/5-fri/nodeWeatherCli/weather.js
@@ -1,5 +1,6 @@
 const request = require('request');
-const api = require('./apiKey.js');
+// const api = require('./apiKey.js');
+const api = process.env.OPEN_WEATHER_MAP_KEY;
 const city = "Melbourne";
 
 request('http://api.openweathermap.org/data/2.5/weather?appid='+api+'&units=metric&q='+ city, { json: true }, (err, res, body) => {
